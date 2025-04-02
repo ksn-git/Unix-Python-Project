1bc8d5bfab8241018669fe0014d8e3dfe884d3ec
PernilleRenkwitz <149146243+PernilleRenkwitz@users.noreply.github.com>
2025-04-02 13:47:31 +0200

    skrev yippie og todo kommentarer

--- test-commit.py
+++ test-commit.py
@@ -1,2 +1,11 @@
 # This is a test
 print("It works!")
+
+print("yippie")
+
+#step by step: 
+#1) lav ændring
+#2) save
+#3) stage din commit ved plus tegn
+#4) skriv en comment hvad du har gjort
+#5) commit
