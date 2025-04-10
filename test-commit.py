0ee6144de95c951df46a4db5882725e075a373af
PernilleRenkwitz <149146243+PernilleRenkwitz@users.noreply.github.com>
2025-04-10 13:32:05 +0200

    Added some incomplete instructions explain git, vscode and repository

--- test-commit.py
+++ test-commit.py
@@ -3,7 +3,17 @@ print("It works!")
 
 print("yippie")
 
-#step by step: 
+## How to add local repository
+#1) have git installed
+#2) something with git command (need to look up again)
+#3) search git clone at the top of vscode
+#4) ... (look up again)
+#5) add link to existing repository in the folder you want it
+#6) need addons?
+
+
+
+#step by step, commit: 
 #1) lav ændring
 #2) save
 #3) stage din commit ved plus tegn
