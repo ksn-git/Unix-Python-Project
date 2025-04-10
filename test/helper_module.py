53162bab73f4a54363166972bb7612e7d596821a
PernilleRenkwitz <149146243+PernilleRenkwitz@users.noreply.github.com>
2025-04-10 10:21:28 +0200

    added file path to helper_module

--- test/helper_module.py
+++ test/helper_module.py
@@ -15,7 +15,17 @@ def add_to_sys_path(relative_path_from_root: str):
     #get target path
     target_path = os.path.join(project_root,relative_path_from_root)
 
-    #add target_path if needed
+    #add target_path if
     if target_path not in sys.path:
         sys.path.insert(0,target_path)
+
+#add file path ('data is hardcoded')
+def get_data_path(filename):
+    """Finds full data path in directory"""
+
+    current_file = os.path.abspath(__file__)
+    project_root = os.path.dirname(os.path.dirname(current_file))
+    return os.path.join(project_root, 'data', filename)
+
+
     
