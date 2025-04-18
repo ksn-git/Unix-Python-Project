e9438beced015dbcab546ffe974ae151da262b43
Karoline Nedergaard <karolinenedergaard@gmail.com>
2025-04-18 12:04:34 +0200

    Gaps possible

--- src/main_program.py
+++ src/main_program.py
@@ -29,8 +29,8 @@ motif_file = get_data_path(motif)     #this is the fasta file, I've added a refe
 infile = open(motif_file,'r')
 
 # Arranging motif into two lists
-# Motif list:   ATCGGATC******AGTCGTTA
-# Penalty list: 8767894200000087698432
+# Motif list:   ATCGGATC*AGTCGTTA
+# Penalty list: 87678942087698432
 motif_list = []
 penalty_list = []
 for line in infile:
@@ -43,10 +43,10 @@ for line in infile:
         # If there is a defined number of unimportant positions
         try:
             unimportant_positions = int(unimportant_positions)
-            for i in range(0, unimportant_positions):
-                motif_list.append("*")
-                penalty_list.append(0)
-                continue 
+            minimum = unimportant_positions
+            maximum = unimportant_positions
+            motif_list.append("*")
+            penalty_list.append(0) 
         except ValueError:
             pass                
         # If there is a range of unimportant positions
@@ -55,9 +55,8 @@ for line in infile:
             if result is not None:
                 minimum = int(result.group(1))
                 maximum = int(result.group(2))
-                for i in range(0, minimum):
-                    motif_list.append("*")
-                    penalty_list.append(0)
+                motif_list.append("*")
+                penalty_list.append(0)
             else:
                 raise ValueError("Invalid unimportant positions format")
                 sys.exit(1)
@@ -103,12 +102,53 @@ def find_motif(sequence, motif_list, penalty_list, max_deviation):
     """ Generator that searches for motif """
     """ Yields position, deviation and sequence when a match is found """
     """ Tries to make a match in each window until max deviation is reached """
+    # Find start position of gap and length of 2nd part of the motif
+    try:
+        star_index = motif_list.index("*")      
+        len_part_2 = len(motif_list) - star_index - 1
+    # If no gaps in motif, ignore
+    except ValueError:
+        star_index = None
 
-    # OBS: ONLY WORKS WHEN EACH POSITION HAS ONE POSSIBLE LETTER
+    # Start searching for matches
     for i in range(0, len(sequence) - len(motif_list)):             # Search until the remaining seq is not long enough to be the motif
-        window = sequence[i:i + len(motif_list)]                    # Window of the same length as the motif
+        window = sequence[i:(i + len(motif_list) + maximum - 1)]    # Window of the same length as the longest possible motif. Len = 29
         deviation = 0
         for j in range(len(window)):
+              
+            # If gap has been reached 
+            # check deviation for 15 gaps, 16 gaps, 17 gaps. Save deviation
+            # Choose gaps corresponding to lowest deviation
+            # Continue searching for match after chosen gap size
+
+            # skip min amount of starts, then min +1 ... up to max amount of starts
+            # motif list should have one star 
+            # while motif life is still long enough, check if equal to window
+
+            # OBS: Motif is currently 29. But j + m(max) = 29, so index error
+            # For gap = 17, k = 17, m = 17-23, j = 6. these are human indices/lengths, not python indices! Change this in code
+            # If the motif contains a gap / star and it has been reached
+            if j == star_index:
+                # Range of gaps 
+                for k in range(minimum, maximum + 1): # range should be the length of the 2nd part of the motif
+                    # check match for the length of the 2nd part of the motif
+                    for m in range(k, k+len_part_2):
+                        # If several possible characters
+                        if isinstance(motif_list[j+m], set):
+                            if window[j+m] not in motif_list[j+m]:
+                                deviation += int(penalty_list[j])
+                                if deviation > max_deviation:
+                                    break
+                        # If one possible character
+                        else: 
+                            if window[j+m] != motif_list[j+m]:
+                                deviation += int(penalty_list[j])
+                                # If the deviation is larger than the max, break out of the loop
+                                if deviation > max_deviation:
+                                    break
+                    if deviation <= max_deviation:
+                        yield((i, deviation, window))
+
             # If several possible characters
             if isinstance(motif_list[j], set):
                 # If the character is not in the list of possible characters, add penalty score
@@ -116,7 +156,8 @@ def find_motif(sequence, motif_list, penalty_list, max_deviation):
                     deviation += int(penalty_list[j])
                     # If the deviation is larger than the max, break out of the loop
                     if deviation > max_deviation:
-                        break                  
+                        break
+
             # If one possible character
             else:
                 if window[j] != motif_list[j]:
@@ -124,6 +165,7 @@ def find_motif(sequence, motif_list, penalty_list, max_deviation):
                     # If the deviation is larger than the max, break out of the loop
                     if deviation > max_deviation:
                         break
+
         # If the deviation is less than the max, yield the match
         if deviation <= max_deviation:
             yield((i, deviation, window))                           # Return the position, deviation and match
