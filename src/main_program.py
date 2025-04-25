67b1e519c59a9bcae972f5edcd991dc31557f324
PernilleRenkwitz <149146243+PernilleRenkwitz@users.noreply.github.com>
2025-04-25 08:33:18 +0200

    removed find_motif but kept use case for it

--- src/main_program.py
+++ src/main_program.py
@@ -72,78 +72,7 @@ for line in infile:
         penalty_list.append(line.strip().split(sep="\t")[1])
 infile.close()
 
-
-def find_motif(sequence, motif_list, penalty_list, max_deviation):
-    """ Generator that searches for motif """
-    """ Yields position, deviation and sequence when a match is found """
-    """ Tries to make a match in each window until max deviation is reached """
-    # Find start position of gap and length of 2nd part of the motif
-    try:
-        star_index = motif_list.index("*")      
-        len_part_2 = len(motif_list) - star_index - 1
-    # If no gaps in motif, ignore
-    except ValueError:
-        star_index = None
-
-    # Start searching for matches
-    for i in range(0, len(sequence) - len(motif_list) - maximum + 1):           # Search until the remaining seq is not long enough to be the motif
-        window = sequence[i:(i + len(motif_list) + maximum - 1)]                # Window of the same length as the longest possible motif. Len = 29
-        deviation = 0
-        for j in range(len(window)):
-              
-            # If gap has been reached 
-            # Check match for all gap sizes. Yield if below max deviation
-
-            # skip min amount of starts, then min +1 ... up to max amount of starts
-            # motif list should have one star 
-            # while motif list is still long enough, check if equal to window
-
-            # For gap = 17: k = 17, m = [17-23], j = 6. these are human indices/lengths, not python indices! Change this in code
-            # If the motif contains a gap / star and it has been reached
-            if j == star_index:
-                # Range of gaps 
-                for k in range(minimum, maximum + 1): # range should be the length of the 2nd part of the motif
-                    # check match for the length of the 2nd part of the motif
-                    for m in range(k, k+len_part_2 - 1):
-                        # If several possible characters
-                        if isinstance(motif_list[j+m-k+1], set):    
-                            if window[j+m] not in motif_list[j+m-k+1]:  
-                                deviation += int(penalty_list[j+m-k+1]) 
-                                if deviation > max_deviation:
-                                    break
-                        # If one possible character 
-                        else: 
-                            if window[j+m] != motif_list[j+m-k+1]:    
-                                deviation += int(penalty_list[j+m-k+1])  
-                                # If the deviation is larger than the max, break out of the loop
-                                if deviation > max_deviation:
-                                    break
-
-            # If gap has been reached, the entire window has already been checked
-            elif star_index is not None and j >= star_index:
-                # Skip this part if the star/gap has already been encountered
-                continue
-
-            elif isinstance(motif_list[j], set):
-                # If the character is not in the list of possible characters, add penalty score
-                if window[j] not in motif_list[j]:
-                    deviation += int(penalty_list[j])
-                    # If the deviation is larger than the max, break out of the loop
-                    if deviation > max_deviation:
-                        break
-
-            # If one possible character
-            else:
-                if window[j] != motif_list[j]:
-                    deviation += int(penalty_list[j])
-                    # If the deviation is larger than the max, break out of the loop
-                    if deviation > max_deviation:
-                        break
-
-        # If the deviation is less than the max, yield the match
-        if deviation <= max_deviation:
-            yield((i, deviation, window))                           # Return the position, deviation and match
-
+#saved use case code for find_motif
 print("Matches are listed as (start position, penalty score, match)")
 print("The header corresponding to the match is printed immediately before the match")
 for header, sequence in fasta:
