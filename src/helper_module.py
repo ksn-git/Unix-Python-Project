e411255cbf653df2b218725df0b7b8d307ab28a9
PernilleRenkwitz <149146243+PernilleRenkwitz@users.noreply.github.com>
2025-04-26 07:57:40 +0200

    moved find_motif into helper_module

--- src/helper_module.py
+++ src/helper_module.py
@@ -105,7 +105,7 @@ def load_motif(motif_file):
     infile.close()
     return motif_list, penalty_list, minimum_gap, maximum_gap
 
-
+#find motif - not fully functional yet!
 def find_motif(sequence, motif_list, penalty_list, max_deviation, minimum_gap, maximum_gap):
     """ Generator that searches for motif """
     """ Yields position, deviation and sequence when a match is found """
@@ -126,28 +126,20 @@ def find_motif(sequence, motif_list, penalty_list, max_deviation, minimum_gap, m
         # If there are gaps in the motif, window is the motif length + maximum number of gaps in motif. 
         # Subtract 1 because there is a star character in the motif list denoting gaps
         else:
-            window = sequence[i:(i + len(motif_list) -1 + maximum_gap)]                # Window of the same length as the longest possible motif. Len = 29
+            window = sequence[i:(i + len(motif_list) - 1 + maximum_gap)]                # Window of the same length as the longest possible motif. Len = 29
         # Reset deviation score and search window for motif
         deviation = 0
         for j in range(len(window)):
             # If gap has been reached 
             # Check match for all gap sizes. Yield if below max deviation
-
-            # skip min amount of stars, then min +1 ... up to max amount of starts
-            # motif list should have one star 
-            # while motif list is still long enough, check if equal to window
-
-            # For gap = 17: k = 17, m = [17-23], j = 6. these are human indices/lengths, not python indices! Change this in code
-            # If the motif contains a gap / star and it has been reached
-            # If TTCAGA*, then j = 6 when star is reached
             if j == star_index:
-                # Range of gaps, e.g. 15, 16, 17
+                # Range of gaps
                 for k in range(minimum_gap, maximum_gap + 1): # range should be the length of the 2nd part of the motif
                     # check match for the length of the 2nd part of the motif
-                    for m in range(k, k+len_part_2 - 1):
-                        print(f"Checking window index: {j + m}")
+                    for m in range(k, k+len_part_2): # maybe minus 1
                         # If several possible characters
-                        if isinstance(motif_list[j+m-k+1], set):    
+                        if isinstance(motif_list[j+m-k+1], set):
+                            print(window[j+m], motif_list[j+m-k+1])    
                             if window[j+m] not in motif_list[j+m-k+1]:  
                                 deviation += int(penalty_list[j+m-k+1]) 
                                 if deviation > max_deviation:
@@ -183,4 +175,4 @@ def find_motif(sequence, motif_list, penalty_list, max_deviation, minimum_gap, m
 
         # If the deviation is less than the max, yield the match
         if deviation <= max_deviation:
-            yield((i, deviation, window))
+            yield((i, deviation, window))                           # Return the position, deviation and match
